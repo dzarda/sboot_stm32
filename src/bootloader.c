97bb0a0808577bcf8bcc39b32c0eb1cb73177bf3
Jaroslav Malec <dzardacz@gmail.com>
2021-09-02 14:44:30 +0200

    Fix dual bank EEPROM build

--- src/bootloader.c
+++ src/bootloader.c
@@ -25,10 +25,10 @@
 #include "crypto.h"
 
 /* Checking for the EEPROM */
-#if defined(DATA_EEPROM_BASE)
+#if defined(DATA_EEPROM_BASE) && defined(DATA_EEPROM_END)
     #define _EE_START    DATA_EEPROM_BASE
     #define _EE_LENGTH   (DATA_EEPROM_END - DATA_EEPROM_BASE + 1)
-#elif defined(FLASH_EEPROM_BASE)
+#elif defined(FLASH_EEPROM_BASE) && defined(FLASH_EEPROM_END)
     #define _EE_START    FLASH_EEPROM_BASE
     #define _EE_LENGTH   (FLASH_EEPROM_END - FLASH_EEPROM_BASE + 1 )
 #endif
